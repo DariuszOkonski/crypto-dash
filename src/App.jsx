4d5c6fdfdf00ca99e3b351be8cf66f009e0f7b61
Dariusz Okonski <darek200180@gmail.com>
2026-09-19 10:13:36 +0200

    add endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-function App() {
+import { useState } from "react";
+const API_URL =
+  "https://api.coingecko.com/api/v3/conins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+
+const App = () => {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   return (
     <div>
       <h1>Crypto Dash</h1>
     </div>
   );
-}
+};
 
 export default App;
